3f396e0d024dd5ab272f50e3fd886bbf5b735666
Przemek <pk.korzen1@gmail.com>
2024-01-21 14:24:18 +0100

    amended update/launch information on the screen

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ wifi_connection.connect_wifi()
 display = Display()
 updates_available = False
 
+def show_checking_update_information():
+    clear_screen()
+    display.oled.text("Checking updates...", 0, 0)
+    show_text()
+
 def show_available_update_information():
     clear_screen()
     display.oled.text("Update available", 0, 0)
-    display.oled.text("Downloading update...", 0, 8)
+    display.oled.text("Downloading", 0, 8)
+    display.oled.text("update... ", 0, 16)
     show_text()
 
 def show_reboot_information():
@@ -22,6 +28,13 @@ def show_reboot_information():
     display.oled.text("Updated to the", 0, 0)
     display.oled.text("latest version!", 0, 8)
     display.oled.text("Rebooting... ", 0, 16)
+    show_text()
+    
+def show_starting_information():
+    clear_screen()
+    display.oled.text("Soft up-to-date!", 0, 0)
+    display.oled.text("Launching", 0, 8)
+    display.oled.text("the station... ", 0, 16)
     show_text()
 
 def clear_screen():
@@ -33,6 +46,7 @@ def show_text():
 OTA = senko.Senko(user="pkkorzen", repo="weather-station-esp32", files=file_config.files)
 #needs to check updates after reboot in case a new file, that was not present before,
 #has been added to file_config.files during an update
+show_checking_update_information()
 if OTA.fetch():
     show_available_update_information()
     sleep(1)
@@ -43,6 +57,7 @@ if OTA.fetch():
     machine.reset()
 
 if not updates_available:
+    show_starting_information()
     updates_available = program.run(updates_available, display, OTA)
 
 if updates_available:
